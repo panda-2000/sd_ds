0cd6c8d1a885af663af83826ee34c8c51d919683
panda-2000 <dan17zh@gmail.com>
2021-11-23 13:52:34 +0300

    2

--- game.py
+++ game.py
@@ -2,12 +2,15 @@
 
 import numpy as np
 
-number = np.random.randint(1, 101) # загадываем число
+
 
 # количество попыток
 count = 0
 
-while True:
+number = np.random.randint(1, 101) # загадываем число
+
+
+while count<20:
     count+=1
     predict_number = int(input("Угадай число от 1 до 100: "))
     
