8e59f93d74cbe8b736e54ce1bbd542e947e6f1b1
panda-2000 <dan17zh@gmail.com>
2021-12-08 21:14:03 +0300

    23

--- просто для практики/parapam.py
+++ просто для практики/parapam.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 countries_data = pd.read_csv('data/melb_data.csv', sep=',')
-# print(countries_data)
+print(countries_data)
 
 print(round(countries_data.loc[3521,'Landsize']/countries_data.loc[1690, 'Landsize']))
+
+
