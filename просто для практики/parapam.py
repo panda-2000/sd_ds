f433445d87cd469da9f77ec5bb8f6ec651729609
panda-2000 <dan17zh@gmail.com>
2022-01-12 14:22:17 +0300

    оао

--- просто для практики/parapam.py
+++ просто для практики/parapam.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-countries_data = pd.read_csv('data/melb_data.csv', sep=',')
-print(countries_data)
-
-print(round(countries_data.loc[3521,'Landsize']/countries_data.loc[1690, 'Landsize']))
-
+sp = pd.read_csv('data/melb_data_ps.csv', sep=',')
+spc=sp.copy()
+spc['Date']=pd.to_datetime(spc['Date'])
+ys=(spc['Address'].loc[9001])
+print(ys)
 
